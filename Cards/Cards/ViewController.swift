cf8c0c84a67718d8c03118364544c19f812b51e5
Jhantelle Belleza <jhantellebelleza@Jhantelles-MacBook-Pro.local>
2016-09-01 13:40:15 -0400

    Completed Program

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -10,10 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     // TODO: Create IB outlets
-
+    
+    @IBOutlet weak var image: UILabel!
+    @IBOutlet weak var number: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // TODO: IB actions and code to update UI
+    
+    
+    @IBAction func spade(sender: AnyObject) {
+        image.text = "♣️"
+        number.text = "4"
+    }
+    
+    @IBAction func flower(sender: AnyObject) {
+        image.text = "♠️"
+        number.text = "3"
+    }
+    
+    @IBAction func diamond(sender: AnyObject) {
+        image.text = "♦️"
+        number.text = "8"
+    }
+    
+    @IBAction func heart(sender: AnyObject) {
+        image.text = "♥️"
+        number.text = "10"
+    }
+    
+
 }
